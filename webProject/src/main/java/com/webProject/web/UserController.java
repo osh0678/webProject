7ed381081d1e439b1199ade4243bf6884ccb38c2
SeongHwan <SeongHwan@DESKTOP-3D3FOJL>
2019-04-20 01:10:57 +0900

    thyleaf, log4j2 연동완료

--- webProject/src/main/java/com/webProject/web/UserController.java
+++ webProject/src/main/java/com/webProject/web/UserController.java
@@ -1,14 +1,22 @@
 package com.webProject.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
 	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println(user);
+	public String create(User user, Model model) {
+		String msg = user.toString();
+		
+		model.addAttribute("msg", msg);
+		logger.info(msg);
+		
 		
 		return "index";
 	}
