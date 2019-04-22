158e7c307f28c95d0e4ae29d1c6d88e5fef0d022
SeongHwan <SeongHwan@DESKTOP-3D3FOJL>
2019-04-22 09:07:04 +0900

    thymeleaf 테스트

--- webProject/src/main/java/com/webProject/web/UserController.java
+++ webProject/src/main/java/com/webProject/web/UserController.java
@@ -1,24 +1,32 @@
 package com.webProject.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private List<User> users = new ArrayList<User>();
 
 	@PostMapping("/create")
 	public String create(User user, Model model) {
-		String msg = user.toString();
 		
-		model.addAttribute("msg", msg);
-		logger.info(msg);
+		users.add(user);
 		
-		
+		logger.info("데이터가 정상적으로 반영 되었습니다.");
 		return "index";
 	}
 
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
+		return "list";
+	}
 }
