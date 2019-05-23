28058bc5652f13ae0b57b33a5664921d73ee4a03
WebPortfolio <osh0678@naver.com>
2019-05-23 13:11:28 +0900

    기본 세팅

---
+++ webProject/src/main/java/com/webProject/domain/UserRepository.java
@@ -0,0 +1,7 @@
+package com.webProject.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
--- webProject/src/main/java/com/webProject/web/UserController.java
+++ webProject/src/main/java/com/webProject/web/UserController.java
@@ -5,30 +5,36 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.webProject.domain.User;
+import com.webProject.domain.UserRepository;
 
 @Controller
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	private List<User> users = new ArrayList<User>();
 
+	@Autowired
+	private UserRepository useRepository;
+	
 	@PostMapping("/create")
 	public String create(User user, Model model) {
 		
 		users.add(user);
-		
-		logger.info("데이터가 정상적으로 반영 되었습니다.");
+		useRepository.save(user);
+		logger.info("user 리스트");
 		return "redirect:/list";
 	}
 
 	@GetMapping("/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
+//		model.addAttribute("users", users);
+		model.addAttribute("users", useRepository.findAll());
 		return "list";
 	}
 }
